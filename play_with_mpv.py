b962d0f5a7eeb156fbc7a4b411509fce5370d724
Ivaylo Iliev <ivailo.iliev@next-consult.com>
2021-01-13 12:07:47 +0200

    Update

--- play_with_mpv.py
+++ play_with_mpv.py
@@ -14,6 +14,7 @@ if '--public' in sys.argv:
 if sys.version_info[0] < 3:  # python 2
     import BaseHTTPServer
     import urlparse
+
     class CompatibilityMixin:
         def send_body(self, msg):
             self.wfile.write(msg+'\n')
@@ -22,6 +23,7 @@ if sys.version_info[0] < 3:  # python 2
 else:  # python 3
     import http.server as BaseHTTPServer
     import urllib.parse as urlparse
+
     class CompatibilityMixin:
         def send_body(self, msg):
             self.wfile.write(bytes(msg+'\n', 'utf-8'))
@@ -35,6 +37,40 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler, CompatibilityMixin):
         if body:
             self.send_body(body)
 
+    def play_with_mpv(self, query):
+        if "list" in query:
+            list_url = 'https://www.youtube.com/playlist?list={}'.format(
+                query["list"][0])
+
+            ytdl_format = ''
+            if query["mpv_args"] is not None:
+                ytdl_format = '--ytdl-format={}'.format(query["mpv_args"][0])
+
+            return Popen([mpv_command, list_url, '--force-window'] +
+                         query.get("mpv_args", []))
+        else:
+            mpv_options = ''
+            return Popen([mpv_command, urls, '--force-window'] +
+                         query.get("mpv_args", []))
+
+    def play_with_celluloid(self, query):
+        if which('celluloid') is not None:
+            # Playlist support
+            if "list" in query:
+                urls += str("&list={}".format(query["list"][0]))
+            mpv_command = 'celluloid'
+            mpv_options = []
+
+            # Translate mpv options to celluloid
+            for mpv_arg in query["mpv_args"]:
+                if '--ytdl-format' in mpv_arg:
+                    mpv_options.append(mpv_arg.replace(
+                        '--ytdl-format=', '--mpv-ytdl-format="') + '"')
+                else:
+                    mpv_options.append(mpv_arg)
+
+            return Popen([mpv_command, urls] + mpv_options)
+
     def do_GET(self):
         try:
             url = urlparse.urlparse(self.path)
@@ -43,43 +79,20 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler, CompatibilityMixin):
             query = {}
         if query.get('mpv_args'):
             print("MPV ARGS:", query.get('mpv_args'))
+            mpv_command = 'mpv'
+            if('mpv_player' in query):
+                mpv_command = query['mpv_player'][0]
         if "play_url" in query:
             urls = str(query["play_url"][0])
             if urls.startswith('magnet:') or urls.endswith('.torrent'):
                 pipe = Popen(['peerflix', '-k',  urls, '--', '--force-window'] +
                              query.get("mpv_args", []))
             else:
-                mpv_command = 'mpv'
-                # Celluloid support.
-                if which('celluloid') is not None:
-                    # Playlist support
-                    if "list" in query:
-                        urls += str("&list={}".format(query["list"][0]))
-                    mpv_command = 'celluloid'
-                    mpv_options = []
+                if mpv_command == 'mpv':
+                    pipe = self.play_with_mpv(query)
+                elif mpv_command == 'celluloid':
+                    pipe = self.play_with_celluloid(query)                   
                     
-                    # Translate mpv options to celluloid
-                    for mpv_arg in query["mpv_args"]:
-                        if '--ytdl-format' in mpv_arg:
-                            mpv_options.append(mpv_arg.replace('--ytdl-format=', '--mpv-ytdl-format="') + '"')
-                        else:
-                            mpv_options.append(mpv_arg)
-                        
-                    pipe = Popen([mpv_command, urls] + mpv_options)
-                else:
-                    if "list" in query:
-                        list_url = 'https://www.youtube.com/playlist?list={}'.format(query["list"][0])
-                        
-                        ytdl_format = ''
-                        if query["mpv_args"] is not None:
-                            ytdl_format = '--ytdl-format={}'.format(query["mpv_args"][0])
-
-                        pipe = Popen([mpv_command, list_url, '--force-window'] +
-                             query.get("mpv_args", []))
-                    else:
-                        mpv_options = ''
-                        pipe = Popen([mpv_command, urls, '--force-window'] +
-                             query.get("mpv_args", []))
             self.respond(200, "playing...")
         elif "cast_url" in query:
             urls = str(query["cast_url"][0])
@@ -123,4 +136,3 @@ def start():
 
 if __name__ == '__main__':
     start()
-
